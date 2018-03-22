c5d6d4c8734a848a3d7961ee5aa36dd97f47df93
monsidev <rsheremetov@list.ru>
2018-03-22 11:30:08 +0300

    add Slider

--- www/assets/main/slider.php
+++ www/assets/main/slider.php
@@ -1,4 +1,4 @@
-<div class="main-slider" id="main-slider">
+<?php /* <div class="main-slider" id="main-slider">
   <div class="main-slider__list">
     <div class="main-slider__item">
       <img src="img/card.png" alt="" class="main-slider__item-image">
@@ -8,5 +8,15 @@
       <img src="img/card-2.png" alt="" class="main-slider__item-image">
       <div class="main-slider__item-text">Дисконтные карты </div>
     </div>
+  </div>
+</div> */ ?>
+<div class="main-slider" id="main-slider">
+  <div class="main-slider__item">
+    <img src="img/card.png" alt="" class="main-slider__item-image">
+    <div class="main-slider__item-text">Дисконтные карты </div>
+  </div>
+  <div class="main-slider__item">
+    <img src="img/card-2.png" alt="" class="main-slider__item-image">
+    <div class="main-slider__item-text">Дисконтные карты </div>
   </div>
 </div>
